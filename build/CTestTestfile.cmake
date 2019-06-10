2dee555b71a21fe6991b195915b872f19a996884
Anton <antos.horbach@gmail.com>
2019-06-10 17:34:01 +0300

    i forgot about gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anton/Code/C++/chess
-# Build directory: /home/anton/Code/C++/chess/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
